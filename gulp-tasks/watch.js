0d2f0c52cb5a66608892672435d52c162e8dc3bd
Joel Venable <joelmusicman@gmail.com>
2018-07-18 17:01:29 -0400

    Completed styling for Hero and Our Start section

---
+++ gulp-tasks/watch.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync-wsl').create();
+
+
+
+gulp.task('watch', function () {
+  browserSync.init({
+    server: {
+      baseDir: "app",
+    },
+    browser: 'chrome'
+  });
+
+  watch([
+    'app/assets/styles/**/*.css', 'app/assets/styles/**/*.scss'
+  ], function () {
+    gulp.start('cssInject');
+  });
+
+  watch('app/index.html', function () {
+    browserSync.reload();
+  });
+});
+
+
+gulp.task('cssInject', ['styles'], function () {
+  return gulp
+    .src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
