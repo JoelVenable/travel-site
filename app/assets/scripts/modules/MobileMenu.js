eb3d9fc0fb8366de804dbda8f59e0cbe2978e55e
Joel Venable <joelmusicman@gmail.com>
2018-07-29 11:19:58 -0400

    finished mobile header and animated menu icon

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -6,6 +6,7 @@ class MobileMenu {
     this.menuIcon = $('.site-header__menu-icon');
     this.menuContent = $('.site-header__menu-content');
     this.events();
+    this.siteHeader = $('.site-header');
 
   }
 
@@ -15,7 +16,11 @@ class MobileMenu {
 
   toggleTheMenu() {
     this.menuContent.toggleClass('site-header__menu-content--is-visible');
+    this.siteHeader.toggleClass('site-header--is-expanded');
+    this.menuIcon.toggleClass('site-header__menu-icon--close-x');
   }
+
+
 }
 
 export default MobileMenu;
